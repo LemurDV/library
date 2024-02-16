37b165af2dd848bb44e48a8a184ebf6e3d7fcee7
Lemur <lemuriosdc@gmail.com>
2024-02-17 07:36:38 +1000

    add drf-yasg (swagger for zoomers)

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from apps.library.views import AuthorsAPIView, BooksAPIView, ExportModelAPIView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/books", BooksAPIView.as_view(), name="books"),
     path("api/v1/authors", AuthorsAPIView.as_view(), name="authors"),
     path("api/v1/export-db", ExportModelAPIView.as_view(), name="export_db"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
